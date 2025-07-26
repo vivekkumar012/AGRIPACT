f61bc24674381a86d9014d45346764bcaacb4f8d
Vivek Kumar <vh118854@gmail.com>
2025-07-27 01:26:38 +0530

    Project Setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi I am full stack developer from Biharrr")
+})
+
+try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDb is connected")
+} catch (error) {
+    res.status(400).json({
+        message: "Error in MongoDB"
+    })
+}
+
+const port = 3001 || process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`App is listening on Port${port}`)
+})
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      Header Page
+    </div>
+  )
+}
+
+export default Header
