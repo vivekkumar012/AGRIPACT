7bab1d84be1706b89103b09f752b3281c95f7584
Vivek Kumar <vh118854@gmail.com>
2025-08-09 03:13:23 +0530

    Router updated

--- backend/Routes/placeRouter.js
+++ backend/Routes/placeRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { loginUser, logout, registerUser } from '../controllers/userController';
+import { createPlace, getAllPlaces, getPlaceById, getUserPlaces, updatePlace } from '../controllers/placeController.js';
+
 
 const placeRouter = express.Router();
 
-placeRouter.post("/register", registerUser);
-placeRouter.post("/login", loginUser);
-placeRouter.get("/getProfile", );
-placeRouter.get("/logout", logout);
+placeRouter.post("/create", createPlace);
+placeRouter.get("/user-places", getUserPlaces);
+placeRouter.get("/places/:id", getPlaceById);
+placeRouter.put("/update-places", updatePlace);
+placeRouter.get("/places", getAllPlaces);
 
 export default placeRouter;  
--- backend/Routes/userRouter.js
+++ backend/Routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getProfile, loginUser, logout, registerUser } from '../controllers/userController.js';
+import isAuthenticate from '../Middlewares/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/getProfile", getProfile);
+userRouter.get("/getProfile", isAuthenticate, getProfile);
 userRouter.get("/logout", logout);
 
 export default userRouter;  
